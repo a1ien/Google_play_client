383a27d440ef297b644ad1fddf4419a0339e22b3
a1ien <esta1ien@gmail.com>
2012-11-13 23:06:16 +0400

    add LICENSE

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,18 @@
+//This file is part of Google Play Client.
+//
+//Google Play Client is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//Foobar is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with Google Play Client.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QFileDialog>
@@ -12,8 +27,8 @@ MainWindow::MainWindow(QWidget *parent)
       downloader(new Downloader(this))
 {
     ui->setupUi(this);
-    connect(session, SIGNAL(MessageSignal(MessageTypes, QString &)), this, SLOT(messageSignalHandler(MessageTypes, QString &)));
-    connect(this, SIGNAL(MessageSignal(MessageTypes, QString &)), this, SLOT(messageSignalHandler(MessageTypes, QString &)));
+    connect(session, SIGNAL(MessageSignal(MessageTypes, QString)), this, SLOT(messageSignalHandler(MessageTypes, QString)));
+    connect(this, SIGNAL(MessageSignal(MessageTypes, QString)), this, SLOT(messageSignalHandler(MessageTypes, QString)));
 }
 
 MainWindow::~MainWindow() {
